24580b345e1e2844d62dc18a7c939b909b2545de
Valeria Vicious <59263802+ValeriaVicious@users.noreply.github.com>
2021-10-12 06:54:32 +0200

    - Стиль приложения - Логика приложения

--- ToDoListApp/ToDoListApp/MainWindow.xaml.cs
+++ ToDoListApp/ToDoListApp/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDoListApp.Models;
+using ToDoListApp.Services;
+
 
 namespace ToDoListApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        private BindingList<ToDoModel> _toDoDataList;
+        private FileIoServices _fileIoServices;
+        private readonly string _path = $"{Environment.CurrentDirectory}\\todoDataList.json";
+
+        #endregion
+
+
+        #region Methods
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIoServices = new FileIoServices(_path);
+
+            try
+            {
+                _toDoDataList = _fileIoServices.LoadData();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+
+            DataGridToDoList.ItemsSource = _toDoDataList;
+            _toDoDataList.ListChanged += ToDoDataList_ListChanged;
+        }
+
+        private void ToDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            try
+            {
+                if (e.ListChangedType == ListChangedType.ItemDeleted ||
+                    e.ListChangedType == ListChangedType.ItemAdded ||
+                    e.ListChangedType == ListChangedType.ItemChanged)
+                {
+                    _fileIoServices.SaveData(sender);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+        }
+
+        #endregion
     }
 }
---
+++ ToDoListApp/ToDoListApp/Services/FileIoServices.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.IO;
+using Newtonsoft.Json;
+using ToDoListApp.Models;
+
+
+namespace ToDoListApp.Services
+{
+    internal sealed class FileIoServices
+    {
+        #region Fields
+
+        private readonly string _path;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public FileIoServices(string path)
+        {
+            _path = path;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public BindingList<ToDoModel> ToDoModelsData()
+        {
+            return null;
+        }
+
+        public void SaveData(object toDoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(_path))
+            {
+                var output = JsonConvert.SerializeObject(toDoDataList);
+                writer.Write((output));
+            }
+        }
+
+        public  BindingList<ToDoModel> LoadData()
+        {
+            var fileExists = File.Exists(_path);
+            if (!fileExists)
+            {
+                File.CreateText(_path).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+
+            using (var reader = File.OpenText(_path))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+
+        #endregion
+    }
+}
